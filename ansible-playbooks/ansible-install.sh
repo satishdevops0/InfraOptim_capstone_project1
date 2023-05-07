1be82773aa5d34f3c2570ce8a9e8dc22e15f6584
Satish Patil <109470620+satishdevops0@users.noreply.github.com>
2023-05-07 13:06:11 +0530

    Delete ansible-playbooks directory

--- ansible-playbooks/ansible-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt -y update
-sudo apt -y install software-properties-common
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt -y install ansible
-ansible --version
-
